5c644a299aad06041eb87b9c083e94267554ccc7
Mathilde André <75432319+mathildeeandre@users.noreply.github.com>
2024-06-01 16:00:09 +0000

    modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mathilde André | PhD student';
+export const SITE_TITLE = 'Mathilde André | Collège de France - University of Vienna';
 export const SITE_DESCRIPTION = 'PhD student in probability applied to population biology';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
